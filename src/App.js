4e9ab3bca8908c7d1661689e63f315a4e36255d3
Christian Lewis <christianlewis024@gmail.com>
2020-10-12 21:27:52 -0700

    styling and layout done, need to source good video links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import Video from "./Video"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi friends</h1>
+ 
+    <div className="app">     
+      <div className="app__videos">
+        <Video/>
+        <Video/>
+        <Video/>
+        <Video/>
+               
+      </div>
+      
+
     </div>
   );
 }
 
 export default App;
+// video component 
+// footer
+// video sidebar ( heart, chat, share)
+// footer(ticker, disk spinner)
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBdW1HU9ov790P_3KRIJQxM3_8pfsOXh-U",
+    authDomain: "dardar-33e2b.firebaseapp.com",
+    databaseURL: "https://dardar-33e2b.firebaseio.com",
+    projectId: "dardar-33e2b",
+    storageBucket: "dardar-33e2b.appspot.com",
+    messagingSenderId: "310048173616",
+    appId: "1:310048173616:web:5df63a854877032aec07f5",
+    measurementId: "G-E5EF2E6LB2"
+  };
